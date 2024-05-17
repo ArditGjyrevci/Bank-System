ffc5184593e7d061decc74abab491855793f3fe8
ArditGjyrevci <arditgjyrevci0298@gmail.com>
2024-05-17 19:57:12 +0200

    BS-1-Added project structure and basic dependencies

---
+++ src/main/java/com/example/Bank_System_Project/BankSystemProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bank_System_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSystemProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSystemProjectApplication.class, args);
+	}
+
+}
